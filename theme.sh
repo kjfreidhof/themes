a1b8099beca39404e7f44daad6714bd65444ba5d
kjfreidhof <89760217+kjfreidhof@users.noreply.github.com>
2022-04-16 12:11:59 -0400

    Add files via upload

---
+++ theme.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+
+tar -xvf Nordic-darker.dar.xz 
+
+cp -r Nordic-darker ~/.themes
+
+cp -r Nordic-darker-v40 ~/.themes
+
+tar -xvf Desert-Dark-icons.tar.gz
+
+cp -r Desert-Dark-icons ~/.icons
+
+
+
+
+
+

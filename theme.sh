12d3be81cba0ebe8f5a4ced74cfc6b27ebe722cd
kjfreidhof <89760217+kjfreidhof@users.noreply.github.com>
2022-04-16 12:31:16 -0400

    Add files via upload

--- theme.sh
+++ theme.sh
@@ -1,12 +1,23 @@
 #!/bin/bash 
 
+# This extracts the theme
+
 tar -xvf Nordic-darker.dar.xz 
+
+# this copies my theme the theme to the home directory 
 
 cp -r Nordic-darker ~/.themes
 
+# this one also copies the theme over to the home directory 
+
 cp -r Nordic-darker-v40 ~/.themes
 
+
+# this extracts the icon
+
 tar -xvf Desert-Dark-icons.tar.gz
+
+# this copies my theme to my icons directory in my home directory 
 
 cp -r Desert-Dark-icons ~/.icons
 
